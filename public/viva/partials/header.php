812238c77d4b34f11407b2d4c9e6cf4aa50b2525
Maarten de Jager <maartendejager@gmail.com>
2016-03-09 11:09:19 +0000

    jquery hack 2

--- public/viva/partials/header.php
+++ public/viva/partials/header.php
@@ -3,6 +3,15 @@
 	<link rel="stylesheet" href="https://www.vivazorggroep.nl/css/20694426/e2967677dc54ad387d485fbad1231bce.css" type="text/css"/>
 	<link rel="stylesheet" href="code/css/wwstyle.css" type="text/css"/>
 
+	<script>
+		var isIE11 = !!(navigator.userAgent.match(/Trident/) && !navigator.userAgent.match(/MSIE/));
+		if (isIE11) {
+		    if (typeof window.attachEvent == "undefined" || !window.attachEvent) {
+		        window.attachEvent = window.addEventListener;
+		    }
+		}
+	</script>
+
     <script type="text/javascript" src="code/js/jquerymin.js"></script>
 	<script type="text/javascript" src="code/js/app.js"></script>
 	<script type="text/javascript" src="code/js/vimeo.js"></script>
