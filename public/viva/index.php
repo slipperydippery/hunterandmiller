7b172b25e948d2d2c09b56eb2b2fba449784bd7d
Maarten de Jager <maartendejager@gmail.com>
2016-03-10 08:17:50 +0000

    reverted

--- public/viva/index.php
+++ public/viva/index.php
@@ -1,40 +1,4 @@
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml"  lang="nl-NL">
-<head itemscope itemtype="http://schema.org/WebSite">
-	<!-- (C) 2016 Snakeware New Media B.V. - www.snakeware.nl - info@snakeware.nl -->
-	<title itemprop="name">ViVa! Zorggroep // Gemeenten</title>
-	<base href="https://www.vivazorggroep.nl/"/>
-	<meta itemprop="url" content="https://www.vivazorggroep.nl/"/>
-	<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
-	<meta http-equiv="content-style-type" content="text/css"/>
-	<meta http-equiv="content-script-type" content="text/javascript"/>
-	<meta http-equiv="content-language" content="nl-NL"/>
-	<meta name="generator" content="Snakeware New Media B.V., e-mail: info@snakeware.nl, internet: http://www.snakeware.nl/"/>
-	<meta name="copyright" content="Copyright (C) 2016 ViVa! Zorggroep"/>
-	<meta name="distribution" content="global"/>
-	<meta name="resource-type" content="document"/>
-	<meta name="verify-v1" content=""/>
-	<meta name="robots" content="index,follow"/>
-	<link rel="home" href="https://www.vivazorggroep.nl/" title="Homepage"/>
-	<link rel="contents" href="https://www.vivazorggroep.nl/sitemap.xml" title="Sitemap"/>
-	<link rel="shortcut icon" href="https://www.vivazorggroep.nl/favicon.ico" type="image/x-icon"/>
-	<script type="text/javascript">var sw={}</script>
-	<link rel="stylesheet" href="css/32538459/e2967677dc54ad387d485fbad1231bce.css" type="text/css"/>
-	<script type="text/javascript">var language=0,browser=8,version=48,steID=1,catID=990,itmID=3692,imgBack=true,rewriting=true,horizontal=true;</script>
-	<script type="text/javascript" src="https://use.typekit.net/gaa3dpy.js"></script>
-	<script type="text/javascript" src="js/36680345/7bceec36e401e4a9033d4d97c051516d.0413.js"></script>
-
-</head>
-<body>
-
-			<iframe frameborder="0" scrolling="no" seamless="seamless" src="https://huntmill.com/viva/gemeenten.php" height="1140" width="960"></iframe>
-
-</body>
-</html>
-<!--
-	Total Time: 0:00:00.053
-	Total database access: 0:00:00.010
-	Main Xsl transformation: 0:00:00.004
-	code: 0:00:00.038
--->
+<?php
+	$current = 'gemeenten';
+	require './partials/content.php';
+?>
