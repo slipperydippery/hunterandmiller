549b3bffe00b69403ac2c0b255bc5a1821aff9a0
Maarten de Jager <maartendejager@gmail.com>
2016-03-10 08:04:57 +0000

    top half commented

--- public/viva/vivaclone.php
+++ public/viva/vivaclone.php
@@ -4,8 +4,9 @@
 <head itemscope itemtype="http://schema.org/WebSite">
 	<!-- (C) 2016 Snakeware New Media B.V. - www.snakeware.nl - info@snakeware.nl -->
 	<title itemprop="name">ViVa! Zorggroep // Gemeenten</title>
-<!-- 	<base href="https://www.vivazorggroep.nl/"/>
-	<meta itemprop="url" content="https://www.vivazorggroep.nl/"/>
+	<base href="https://www.vivazorggroep.nl/"/>
+
+<!-- 	<meta itemprop="url" content="https://www.vivazorggroep.nl/"/>
 	<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
 	<meta http-equiv="content-style-type" content="text/css"/>
 	<meta http-equiv="content-script-type" content="text/javascript"/>
@@ -13,7 +14,8 @@
 	<meta name="generator" content="Snakeware New Media B.V., e-mail: info@snakeware.nl, internet: http://www.snakeware.nl/"/>
 	<meta name="copyright" content="Copyright (C) 2016 ViVa! Zorggroep"/>
 	<meta name="distribution" content="global"/>
-	<meta name="resource-type" content="document"/>
+	<meta name="resource-type" content="document"/> -->
+	
 	<meta name="verify-v1" content=""/>
 	<meta name="robots" content="index,follow"/>
 	<link rel="home" href="https://www.vivazorggroep.nl/" title="Homepage"/>
@@ -22,7 +24,8 @@
 	<script type="text/javascript">var sw={}</script>
 	<link rel="stylesheet" href="css/32538459/e2967677dc54ad387d485fbad1231bce.css" type="text/css"/>
 	<script type="text/javascript">var language=0,browser=8,version=48,steID=1,catID=990,itmID=3692,imgBack=true,rewriting=true,horizontal=true;</script>
-	<script type="text/javascript" src="https://use.typekit.net/gaa3dpy.js"></script> -->
+
+	<script type="text/javascript" src="https://use.typekit.net/gaa3dpy.js"></script>
 	<script type="text/javascript" src="js/36680345/7bceec36e401e4a9033d4d97c051516d.0413.js"></script>
 	<script type="text/javascript">try{Typekit.load();}catch(e){};
 var imgImageLoader = new Responsive.ImageLoader("img[data-responsive]", Responsive.ImageLoader.setImgSource);
