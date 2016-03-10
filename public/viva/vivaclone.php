e8577271b1e782622b81ccca66a1ba4681da3508
Maarten de Jager <maartendejager@gmail.com>
2016-03-10 07:59:37 +0000

    partially working

--- public/viva/vivaclone.php
+++ public/viva/vivaclone.php
@@ -24,11 +24,11 @@
 	<script type="text/javascript">var language=0,browser=8,version=48,steID=1,catID=990,itmID=3692,imgBack=true,rewriting=true,horizontal=true;</script>
 	<script type="text/javascript" src="https://use.typekit.net/gaa3dpy.js"></script>
 	<script type="text/javascript" src="js/36680345/7bceec36e401e4a9033d4d97c051516d.0413.js"></script> -->
-<!-- 	<script type="text/javascript">try{Typekit.load();}catch(e){};
+	<script type="text/javascript">try{Typekit.load();}catch(e){};
 var imgImageLoader = new Responsive.ImageLoader("img[data-responsive]", Responsive.ImageLoader.setImgSource);
 var bgImageLoader = new Responsive.ImageLoader("[data-responsiveBg]", Responsive.ImageLoader.setBgSource);
 sw.minSearchLen=4;
-</script> -->
+</script>
 <script type="text/javascript">
 (function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
