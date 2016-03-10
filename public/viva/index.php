43b607b10e59d5fa686a45d51a91fd8279d2ba88
Maarten de Jager <maartendejager@gmail.com>
2016-03-10 08:14:16 +0000

    super hack

--- public/viva/index.php
+++ public/viva/index.php
@@ -1,4 +1,54 @@
-<?php
-	$current = 'gemeenten';
-	require './partials/content.php';
-?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"  lang="nl-NL">
+<head itemscope itemtype="http://schema.org/WebSite">
+	<!-- (C) 2016 Snakeware New Media B.V. - www.snakeware.nl - info@snakeware.nl -->
+	<title itemprop="name">ViVa! Zorggroep // Gemeenten</title>
+	<base href="https://www.vivazorggroep.nl/"/>
+	<meta itemprop="url" content="https://www.vivazorggroep.nl/"/>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+	<meta http-equiv="content-style-type" content="text/css"/>
+	<meta http-equiv="content-script-type" content="text/javascript"/>
+	<meta http-equiv="content-language" content="nl-NL"/>
+	<meta name="generator" content="Snakeware New Media B.V., e-mail: info@snakeware.nl, internet: http://www.snakeware.nl/"/>
+	<meta name="copyright" content="Copyright (C) 2016 ViVa! Zorggroep"/>
+	<meta name="distribution" content="global"/>
+	<meta name="resource-type" content="document"/>
+	<meta name="verify-v1" content=""/>
+	<meta name="robots" content="index,follow"/>
+	<link rel="home" href="https://www.vivazorggroep.nl/" title="Homepage"/>
+	<link rel="contents" href="https://www.vivazorggroep.nl/sitemap.xml" title="Sitemap"/>
+	<link rel="shortcut icon" href="https://www.vivazorggroep.nl/favicon.ico" type="image/x-icon"/>
+	<script type="text/javascript">var sw={}</script>
+	<link rel="stylesheet" href="css/32538459/e2967677dc54ad387d485fbad1231bce.css" type="text/css"/>
+	<script type="text/javascript">var language=0,browser=8,version=48,steID=1,catID=990,itmID=3692,imgBack=true,rewriting=true,horizontal=true;</script>
+	<script type="text/javascript" src="https://use.typekit.net/gaa3dpy.js"></script>
+	<script type="text/javascript" src="js/36680345/7bceec36e401e4a9033d4d97c051516d.0413.js"></script>
+	<script type="text/javascript">try{Typekit.load();}catch(e){};
+var imgImageLoader = new Responsive.ImageLoader("img[data-responsive]", Responsive.ImageLoader.setImgSource);
+var bgImageLoader = new Responsive.ImageLoader("[data-responsiveBg]", Responsive.ImageLoader.setBgSource);
+sw.minSearchLen=4;
+</script>
+<script type="text/javascript">
+(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,"script","//www.google-analytics.com/analytics.js","ga");
+ga("create","UA-48315816-1","auto");
+ga("require","displayfeatures");
+ga("require","linkid", "linkid.js");
+ga("send","pageview");
+</script>
+</head>
+<body onload="bodyLoad(this,false)" style="text-align:left">
+
+			<iframe frameborder="0" scrolling="no" seamless="seamless" src="https://huntmill.com/viva/" height="1140" width="960"></iframe>
+
+</body>
+</html>
+<!--
+	Total Time: 0:00:00.053
+	Total database access: 0:00:00.010
+	Main Xsl transformation: 0:00:00.004
+	code: 0:00:00.038
+-->
