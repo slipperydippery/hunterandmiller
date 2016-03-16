61b4b4b682ac0e994557ae2c8a4791222af6ed53
Maarten de Jager <maartendejager@gmail.com>
2016-03-16 11:32:18 +0100

    returning document domain https test

--- public/viva/code/js/vimeo.js
+++ public/viva/code/js/vimeo.js
@@ -1,21 +1,29 @@
 $(function() {
+    console.log('---start---')
     var player = $('iframe.vimeo');
-    console.log(player);
-    var playerOrigin = '*';
-    var status = $('.status');
+
+    document.getElementById("vimeoiframe").onload = function() {
+      console.log("#vimeoiframe is loaded");
+        console.log(player);
+        console.log(player[0].contentWindow);
+        console.log(player[0].contentWindow.domain);
+        var playerOrigin = '*';
+        var status = $('.status');
 
 
-    // Listen for messages from the player
-    console.log('hello, this is your msie version if applicable: ' + detectIE());
-    var vimeoapp = document.getElementById("vimeoapp");
-    if (window.addEventListener) {
-        window.addEventListener('message', onMessageReceived, false);
-        console.log('window.addEventListener onMessageReceived(event) ');
-    }
-    else {
-    	console.log('addEventListener-else; atach event');                   //IE < 9
-        window.attachEvent('onmessage', onMessageReceived, false);
-    }
+        // Listen for messages from the player
+        console.log('hello, this is your msie version if applicable: ' + detectIE());
+        var vimeoapp = document.getElementById("vimeoapp");
+        if (window.addEventListener) {
+            window.addEventListener('message', onMessageReceived, false);
+            console.log('window.addEventListener onMessageReceived(event) ');
+        }
+        else {
+        	console.log('addEventListener-else; atach event');                   //IE < 9
+            window.attachEvent('onmessage', onMessageReceived, false);
+        }
+    };
+
 
     // Handle messages received from the player
     function onMessageReceived(event) {
